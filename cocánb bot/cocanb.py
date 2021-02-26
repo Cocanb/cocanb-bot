a0abd011ca42761719a73d80ee6dc35229c0896a
AnastasiosZ <46845162+AnastasiosZ@users.noreply.github.com>
2021-02-26 21:59:21 +0200

    Update cocanb.py

--- cocánb bot/cocanb.py
+++ cocánb bot/cocanb.py
@@ -20,15 +20,16 @@ class Cocanb(commands.Cog):
       await ctx.send ('Invalid format')
 
   @bot.command(name="toc", help="Translate from any language into Cocanb")
-  async def toc (self, ctx, *, *args):
-    sentences=''.join(args)
-    if sentences[-1] == '.':
-        sentences= sentences[:-1]
-    sens = sentences.split('.')
-    
+  async def toc (self, ctx, *args):
+    arg = ' '.join(args)
+
+    final = ""
+    if arg[-1] == '.':
+        arg= arg[:-1]
+    sens = arg.split('.')
     for sen in sens:
-        final = final + t.toCocanb(sen) + ". "
-        await ctx.send(final)
+        final = final + toc.toCocanb(sen) + ". "
+    await ctx.send(final)
       
   @bot.command (name="script", help= "Sends the Cocánb symbols\nSupported: cocanb/cocánb, cock, and, ball, torture, shit, cringe, constriction, onomatopoeia/onomatopœia, altort\n(Words separated with / output the same thing)")
   async def script(self, ctx, *, word):
